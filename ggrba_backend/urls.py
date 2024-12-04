66e0cbfa3c8ccec7fc616bee4712f2ca1ced6a7e
Ilhan <ilhanmuftic@gmail.com>
2024-12-04 17:44:14 +0100

    set up, ok, autoreload

--- ggrba_backend/urls.py
+++ ggrba_backend/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
+from ggrba_backend.Views.OkView import OkView
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('ok', OkView.as_view(), name="ok")
 ]
